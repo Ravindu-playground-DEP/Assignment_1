b2fa1172e7360d6787e58b044f572bcfde2cbfd0
Ravindu <ravindumadusanka998@gmail.com>
2023-07-31 22:51:49 +0530

    Complete Task 6-A

---
+++ Demo5.java
@@ -0,0 +1,11 @@
+public class Demo5 {
+    public static void main(String[] args) {
+        int isbn1 = 978;
+        int isbn2 = 3;
+        int isbn3 = 16;
+        int isbn4 = 1484100;
+
+        System.out.printf("\033[31m%d\033[0m-\033[32m%d\033[0m-\033[33m%d\033[0m-\033[034m%d\033[0m \n", isbn1, isbn2, isbn3, isbn4);
+ 
+    }
+}
